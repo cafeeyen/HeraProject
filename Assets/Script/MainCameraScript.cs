d54486fc70e0641a135826b37599be2422b4b18c
cafeeyen <s7070113@kmitl.ac.th>
2017-03-15 13:53:50 +0700

    Add Map03
    - Add new scene, terrain, texture, tree

---
+++ Assets/Script/MainCameraScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCameraScript : MonoBehaviour {
+
+	public GameObject player;
+    private Vector3 offset;
+    private float X_CAMERA = 0, Y_CAMERA = 40, Z_CAMERA = -40;
+
+    
+    void Start () 
+    {
+        transform.position = new Vector3(player.transform.position.x, player.transform.position.y + Y_CAMERA, player.transform.position.z + Z_CAMERA);
+        offset = transform.position - player.transform.position;
+    }
+    
+    void LateUpdate () 
+    {
+        transform.position = player.transform.position + offset;
+    }
+}
