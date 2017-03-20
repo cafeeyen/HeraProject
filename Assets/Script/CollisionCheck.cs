3afa92522329adfdd5d7cf76d03677ca8356b3cc
cafeeyen <s7070113@kmitl.ac.th>
2017-03-21 00:21:55 +0700

    Change collision condition
    - Remove rigidbody from character
    - Use OnControllerColliderHit
    - Increase Skin Width to 1

--- Assets/Script/CollisionCheck.cs
+++ Assets/Script/CollisionCheck.cs
@@ -2,41 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CollisionCheck : MonoBehaviour {
+public class CollisionCheck : MonoBehaviour
+{
 
     private DieAndRespawn dieAndRespawn;
     private PlayerInventory pInventory;
 
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
 		dieAndRespawn = gameObject.GetComponent<DieAndRespawn>();
         pInventory = gameObject.GetComponent<PlayerInventory>();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 
-    
-
-	void OnCollisionEnter (Collision col)
+	void OnControllerColliderHit(ControllerColliderHit hit)
     {
-        if (col.gameObject.tag == "Water")
+        Rigidbody body = hit.collider.attachedRigidbody;
+        if (body == null || body.isKinematic)
+            return;
+        else if (body.gameObject.tag == "Water")
         {
             Debug.Log("Collide");
-            //Destroy(col.gameObject);
             dieAndRespawn.OnDied();
         }
-        //player pick item here
-        if (col.gameObject.tag == "Item01")
+
+        //Player pickup item here
+        else if (body.gameObject.tag == "Item01")
         {
             Debug.Log("Got Item01");
-            pInventory.addItem(col.gameObject.GetComponent<Item01>());
+            pInventory.addItem(body.gameObject.GetComponent<Item01>());
+            Destroy(body.gameObject);
         }
     }
-
-    void OnCollisionExit (Collision col){}
-
 }
--- Assets/Script/Item01.cs
+++ Assets/Script/Item01.cs
@@ -3,56 +3,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Item01 : MonoBehaviour, Items {
+public class Item01 : MonoBehaviour, Items
+{
 
-	private string itemName = "Item01";
-	private string itemDescription = "simple Water that deng dueng";
+    private string itemName = "Item01";
+    private string itemDescription = "simple Water that deng dueng";
 
     private int itemQuantity = 0;
 
-	public string names
+    public string names
     {
-        get{return itemName;}
+        get { return itemName; }
 
-        set{ itemName = value;}
-        
-    } 
-	
-    public string description 
+        set { itemName = value; }
+
+    }
+
+    public string description
     {
         get { return itemDescription; }
 
-        set{ itemDescription = value;}
-    } 
+        set { itemDescription = value; }
+    }
 
     public int quantity
     {
         get { return itemQuantity; }
 
-        set{ itemQuantity = value;}
+        set { itemQuantity = value; }
     }
-
-    void OnCollisionEnter(Collision col)
-    {
-        if (col.gameObject.name == "cHIBI_bone")
-        {
-            Destroy(gameObject);
-        }
-    }
-
-    void OnCollisionExit(Collision col)
-    {
-    }
-
-
-	// Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
 }
