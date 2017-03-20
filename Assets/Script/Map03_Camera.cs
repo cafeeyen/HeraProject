f978c5894da1275cec536d40fa427993f9d57266
cafeeyen <s7070113@kmitl.ac.th>
2017-03-20 19:14:46 +0700

    Change camera position

--- Assets/Script/Map03_Camera.cs
+++ Assets/Script/Map03_Camera.cs
@@ -7,7 +7,7 @@ public class Map03_Camera : MonoBehaviour
     public GameObject player;
     private Vector3 offset;
     private float angle_x, angle_y;
-    private float X_CAMERA = 0, Y_CAMERA = 40, Z_CAMERA = -40;
+    private float X_CAMERA = 0, Y_CAMERA = 60, Z_CAMERA = -60;
 
     void Start()
     {
@@ -24,14 +24,15 @@ public class Map03_Camera : MonoBehaviour
 
             // Base Height - ( Max angle - current angle ) / Weight(speed & min height)
             offset.y = Y_CAMERA - (45 - angle_x) / 2;
+            offset.z = Z_CAMERA - (45 - angle_x) / 2;
         }
         else if (player.transform.position.z >= 820 && player.transform.position.z <= 940 && player.transform.position.x <= 1060)
         {
             angle_y = ((940 - player.transform.position.z) / (120f / 90f)) - 90;
             transform.rotation = Quaternion.Euler(10, angle_y, 0);
 
-            offset.x = X_CAMERA + (0 - angle_y) / (90f / 60f);
-            offset.z = Z_CAMERA + (0 - angle_y) / (90f / 40f);
+            offset.x = X_CAMERA + (0 - angle_y);
+            offset.z = Z_CAMERA - ((45 - angle_x) / 2) + (0 - angle_y) + angle_y/15;
         }
         transform.position = player.transform.position + offset;
     }
