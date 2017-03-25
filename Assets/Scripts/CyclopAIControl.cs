05f4c7d970e69e5744570d192d0f6a371aceac9e
fasterac <s7070061@kmitl.ac.th>
2017-03-25 17:07:30 +0700

    Harpy monster AI
    -fix some cyclop xz rotetion
    -harpy now not have hover action during dash

--- Assets/Scripts/CyclopAIControl.cs
+++ Assets/Scripts/CyclopAIControl.cs
@@ -4,20 +4,18 @@ using UnityEngine;
 
 public class CyclopAIControl : MonoBehaviour {
 
-	 //public GameObject target;
 	 private Animator animator;
 	 public CharacterController control;
 	 public GameObject player;
  
-     public int followRange;
-	 public int hitRange;
-	 public int dashRange;
-	 public float moveSpeed, maxDashTime, maxHitTime;
+     public int gravity, followRange, hitRange, dashRange;
+	 public float moveSpeed, turnSpeed, maxDashTime, maxHitTime;
 
-	 private Vector3 moveVector;
-	 private int cyclopSkill = 1;
-	 private bool dashFlag = false, inRange =false;
-	 private float currentDashTime = 0, currentHitTime = 0, currentSpeed;
+	 private Vector3 moveVector, playerRotation, monsterRotation;
+	 private bool isColliding = false, inRange =false;
+	 private float currentDashTime = 0, currentHitTime = 0, currentSpeed, dashCoolDownCounter, hitCoolDownCounter, dashCoolDown = 8, hitCoolDown = 1;
+	 private enum CyclopAction {Neutral, Attacking, Hitting, Dashing}
+	 private CyclopAction cyclopAction;
 
 	// Use this for initialization
 	void Start () {
@@ -25,6 +23,8 @@ public class CyclopAIControl : MonoBehaviour {
 		control = gameObject.GetComponent<CharacterController>();
 		player = GameObject.FindWithTag("Player");
 		currentSpeed = moveSpeed;
+		cyclopAction = CyclopAction.Neutral;
+		animator.SetInteger("attacking", 0);
 	}
 	
 	// Update is called once per frame
@@ -33,67 +33,74 @@ public class CyclopAIControl : MonoBehaviour {
 		float distance = Vector3.Distance(transform.position, player.transform.position);
 		inRange = distance < followRange;
 
-		if(animator.GetInteger("attacking") == 0 && distance < followRange){
+		if(cyclopAction == CyclopAction.Neutral && inRange){
 			animator.SetInteger("attacking", 1);
+			cyclopAction = CyclopAction.Attacking;
 		}
 		
 		if(inRange){
-			//Debug.Log(distance + " " + animator.GetInteger("attacking"));
-			if(animator.GetInteger("attacking") == 1){
-				transform.LookAt(new Vector3( player.transform.position.x, 0, player.transform.position.z ));
-				moveVector = new Vector3(Vector3.forward.x * Time.deltaTime * currentSpeed, 0, Vector3.forward.z * Time.deltaTime * currentSpeed);
-
-				if(cyclopSkill == 1){
-					if(distance < dashRange && currentDashTime == 0){
-						animator.SetInteger("attacking", 2);
-						control.Move(new Vector3(0, 2, 0));
-						currentSpeed += 15;
-						Debug.Log(" Dashhhh-- ");
-					}
-					if(currentDashTime != 0){
-						currentDashTime = 0;
-					}
-				}
-				
-				if(cyclopSkill == 2){
-					
-					if(distance < hitRange){
-						Debug.Log(" POK! ");
-						moveVector = Vector3.zero;
-						animator.SetInteger("hitting", 1);
-						currentHitTime += 1;
-					}
-					if(currentHitTime < maxHitTime && currentHitTime > 0){
-						currentHitTime += 1;
-						moveVector = Vector3.zero;
-					}
-					if(currentHitTime > maxHitTime){
-						animator.SetInteger("hitting", 0);
-						currentHitTime = 0;
-						currentDashTime = 0;
-						cyclopSkill = 1;
-					}
-				}
-				transform.Translate(moveVector);
-			}
-			else if(animator.GetInteger("attacking") == 2){
-				transform.Translate(Vector3.forward.x * Time.deltaTime * currentSpeed, 0, Vector3.forward.z * Time.deltaTime * currentSpeed);
-				currentDashTime += 1;
-				if(currentDashTime > maxDashTime ){
+			///Debug.Log(cyclopAction);
+			if(cyclopAction == CyclopAction.Hitting){
+				animator.SetInteger("hitting", 1);
+				moveVector = Vector3.zero;
+				currentHitTime += 1;
+				if(currentHitTime > maxHitTime){
+					currentHitTime = 0;
+					animator.SetInteger("hitting", 0);
 					animator.SetInteger("attacking", 1);
-					currentSpeed -= 15;
-					currentDashTime = 0;
-					cyclopSkill = 2;
+					cyclopAction = CyclopAction.Attacking;
 				}
 			}
-			
-			
+			else if(cyclopAction == CyclopAction.Dashing){
+				moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
+				animator.SetInteger("attacking", 2);
+				currentDashTime += 1;
+				if(currentDashTime > maxDashTime){ //|| isColliding
+					currentDashTime = 0;
+					currentSpeed -= 30;
+					cyclopAction = CyclopAction.Attacking;
+				}
+			}
+			else if(cyclopAction == CyclopAction.Attacking){
+				animator.SetInteger("attacking", 1);
+				moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
+				monsterRotation = new Vector3(transform.position.x, 0, transform.position.z);
+				playerRotation = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+				transform.rotation = Quaternion.Slerp(transform.rotation, 
+					Quaternion.LookRotation(playerRotation - monsterRotation), turnSpeed * Time.deltaTime);
+				if(distance < 1){ //in normal mode, very near cant walk
+					moveVector = Vector3.zero;
+				}
+				if(distance < hitRange && Time.time > hitCoolDownCounter){
+					hitCoolDownCounter = Time.time + hitCoolDown;
+					currentHitTime = 0;
+					cyclopAction = CyclopAction.Hitting;
+				}
+				else if(distance < dashRange && Time.time > dashCoolDownCounter){
+					dashCoolDownCounter = Time.time + dashCoolDown;
+					currentSpeed += 30;
+					currentDashTime = 0;
+					cyclopAction = CyclopAction.Dashing;
+				}
+			}			
 		}
-		else
-        {
+		else if(!inRange){
 			animator.SetInteger("attacking", 0);
 			currentSpeed = moveSpeed;
+			cyclopAction = CyclopAction.Neutral;
+			moveVector = Vector3.zero;
 		}
-		
+		moveVector.y -= gravity * Time.deltaTime;
+		control.Move(moveVector);
 	}
+
+	void OnControllerColliderHit(ControllerColliderHit hit)
+    {
+		if(hit.gameObject.tag == "Player" || hit.gameObject.tag == "Wall"){
+			isColliding = true;
+		}
+		else{
+			isColliding = false;
+		}
+    }
 }
---
+++ Assets/Scripts/HarpyAIController.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HarpyAIController : MonoBehaviour {
+
+	private Animator animator;
+	public CharacterController control;
+	public GameObject player;
+
+	public int followRange, dashRange;
+	public float height, moveSpeed, turnSpeed, maxDashTime, dashCooldown = 10;
+	private float distance, currentSpeed, currentDashTime, dashCooldownCounter;
+	private bool inRange;
+	private Vector3 moveVector, playerXZPosition, harpyXZPosition;
+	private enum HarpyAction {Neutral, Following, Dashing}
+	private HarpyAction harpyAction;
+	
+
+	// Use this for initialization
+	void Start () {
+		animator = gameObject.GetComponentInChildren<Animator>();
+		control = gameObject.GetComponent<CharacterController>();
+		player = GameObject.FindWithTag("Player");
+		currentSpeed = moveSpeed;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		distance = Vector3.Distance(transform.position, player.transform.position);
+		inRange = distance < followRange;
+		moveVector.y = (height - transform.position.y)*Time.deltaTime; //slowly move to default height
+
+		if(harpyAction == HarpyAction.Neutral && inRange){
+			animator.SetInteger("flying", 0);
+			harpyAction = HarpyAction.Following;
+		}
+		if(inRange){
+			if(harpyAction == HarpyAction.Following){
+				animator.SetInteger("flying", 0);
+				moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
+				harpyXZPosition = new Vector3(transform.position.x, 0, transform.position.z);
+				playerXZPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+				transform.rotation = Quaternion.Lerp(transform.rotation, 
+					Quaternion.LookRotation(playerXZPosition - harpyXZPosition), turnSpeed * Time.deltaTime);
+						moveVector.y = (height - transform.position.y)*Time.deltaTime; 
+				if(distance < 2){ 
+					moveVector = Vector3.zero;
+				}				
+				
+				if(distance < dashRange && Time.time > dashCooldownCounter){
+					harpyAction = HarpyAction.Dashing;
+					currentDashTime = 0;
+					currentSpeed += 40;
+				}
+			}
+			else if(harpyAction == HarpyAction.Dashing){
+				moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
+				currentDashTime += 1;
+				
+				if(currentDashTime > maxDashTime){
+					animator.SetInteger("flying", 0);
+					currentSpeed -= 40;
+					dashCooldownCounter = Time.time + dashCooldown;
+					harpyAction = HarpyAction.Following;
+				}
+				else if(currentDashTime+(maxDashTime*0.2)> maxDashTime){
+					animator.SetInteger("flying", 3);
+				}
+				else if(currentDashTime < 40){
+					animator.SetInteger("flying", 1);
+					moveVector = Vector3.zero;
+					harpyXZPosition = new Vector3(transform.position.x, 0, transform.position.z);
+					playerXZPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+					transform.rotation = Quaternion.Lerp(transform.rotation, 
+						Quaternion.LookRotation(playerXZPosition - harpyXZPosition), turnSpeed * Time.deltaTime);
+				}
+				else if(currentDashTime < maxDashTime){
+					animator.SetInteger("flying", 2);
+					moveVector.y = ((player.transform.position.y-5) - transform.position.y) * Time.deltaTime;
+				}
+			}
+		}
+		else if(!inRange){
+			animator.SetInteger("flying", 0);
+			currentSpeed = moveSpeed;
+			harpyAction = HarpyAction.Neutral;
+			moveVector = Vector3.zero;
+		}
+		control.Move(moveVector);
+	}
+}
