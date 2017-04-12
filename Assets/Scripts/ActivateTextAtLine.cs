249fdc00d620cd24d3fbfb2d40a9ebce3c6f26a3
fasterac <s7070061@kmitl.ac.th>
2017-04-12 22:58:54 +0700

    Add Dialog gui and Save mode
    -Dialog start at scene 1
    -collide with foundtain, fountain will say somthing
    -ESC for open menu
    -now can use only save
    -load will clash game
    -save dir in ProjectDir/Asset

---
+++ Assets/Scripts/ActivateTextAtLine.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivateTextAtLine : MonoBehaviour {
+
+	public TextAsset theText;
+	public TextBoxManager textBoxManager;
+	public int startLine, endLine;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	private void OnTriggerEnter(Collider other)
+    {
+		Debug.Log(other.tag);
+        if(other.gameObject.CompareTag("Player"))
+            textBoxManager.reloadScript(theText);
+			textBoxManager.currentLine = startLine;
+			textBoxManager.endAtLine = endLine;
+			textBoxManager.enableTextBox();
+    }
+}
---
+++ Assets/Scripts/TextBoxManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBoxManager : MonoBehaviour {
+	public GameObject textBox;
+	public Text theText, speakerName;
+	public TextAsset textFile;
+	public string[] textLines;
+
+	public int currentLine, endAtLine;
+	public bool isTextboxActive;
+	public CharacterControl player;
+	
+
+	// Use this for initialization
+	void Start () 
+	{
+		//player = FindObjectOfType<CharacterControl>();
+		if(textFile != null)
+		{
+			textLines = (textFile.text.Split('\n'));
+		}
+
+		if(endAtLine == 0)
+		{
+			endAtLine = textLines.Length - 1;
+		}
+		
+		if(isTextboxActive){enableTextBox();}
+		else{disableTextBox();}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(!isTextboxActive)
+		{
+			return;
+		}
+		
+		if(!(currentLine > endAtLine)){
+			theText.text = textLines[currentLine].Split('|')[1];
+			speakerName.text = textLines[currentLine].Split('|')[0];
+		}
+
+		if(currentLine > endAtLine){
+			disableTextBox();
+		}
+
+		if(Input.GetKeyDown(KeyCode.Space) && isTextboxActive){
+			currentLine += 1;
+		}
+
+	}
+
+	public void enableTextBox()
+	{
+		isTextboxActive = true;
+		textBox.SetActive(true);
+		player.enabled = false;
+	}
+
+	public void disableTextBox()
+	{
+		isTextboxActive = false;
+		textBox.SetActive(false);
+		player.enabled = true;
+	}
+
+	public void reloadScript(TextAsset theText)
+	{
+		if(theText != null)
+		{
+			textLines = new string[1];
+			textLines = (theText.text.Split('\n'));
+		}
+	}
+}
