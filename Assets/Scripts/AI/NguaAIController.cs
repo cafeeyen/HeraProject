36a873c62396e5ba419c89912b4aa561eebd2a74
fasterac <s7070061@kmitl.ac.th>
2017-04-27 22:49:19 +0700

    Ngua Edited v02
    -no more collision
    -stop when collide with player
    -3move with difference range and cooldown

--- Assets/Scripts/AI/NguaAIController.cs
+++ Assets/Scripts/AI/NguaAIController.cs
@@ -2,136 +2,171 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NguaAIController : MonoBehaviour {
+public class NguaAIController : MonoBehaviour
+{
 
-	private Animator animator;
-	public CharacterController control;
-	public GameObject player;
-	public Collider headCollider;
+    private Animator animator;
+    public CharacterController control;
+    public GameObject player;
+    public Collider headCollider;
 
-	public int followRange, attackRange, gravity;
-	public float moveSpeed, turnSpeed, maxDashTime, cooldown = 1;
-	private float distance, currentSpeed, currentDashTime, cooldownCounter;
-	private bool inRange;
-	private Vector3 moveVector, playerXZPosition, nguaXZPosition;
-	private enum NguaAction {Neutral, Following, Punching, Dashing, TailAttack}
-	private NguaAction nguaAction;
+    public int followRange, attackRange, slapRange = 8, tailRange = 12, dashRange = 25, gravity;
+    public float moveSpeed, turnSpeed, maxDashTime, slapCooldown = 1.55f, tailCooldown = 3.5f, dashCooldown = 5.5f;
+    private float distance, currentSpeed, currentDashTime, slapCooldownCounter, tailCooldownCounter, dashCooldownCounter;
+    private bool inRange;
+    private Vector3 moveVector, playerXZPosition, nguaXZPosition;
+    private enum NguaAction { None, Slapping, Dashing, TailAttack }
+    private enum NguaMoving { Neutral, Following, Attacking }
+    private NguaAction nguaAction;
+    private NguaMoving nguaMoving;
 
-	// Use this for initialization
-	void Start () {
-		animator = gameObject.GetComponentInChildren<Animator>();
-		control = gameObject.GetComponent<CharacterController>();
-		
-		player = GameObject.FindWithTag("Player");
-		nguaAction = NguaAction.Neutral;
-		currentSpeed = moveSpeed;
-		headCollider.enabled = false;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		distance = Vector3.Distance(transform.position, player.transform.position);
-		inRange = distance < followRange;
-		if(inRange)
-		{
-			Debug.Log(nguaAction + " " + distance );
-			if(nguaAction == NguaAction.Following)
-			{
-				animator.SetInteger("waking", 1);
-				moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
-				nguaXZPosition = new Vector3(transform.position.x, 0, transform.position.z);
-				playerXZPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
-				transform.rotation = Quaternion.Lerp(transform.rotation, 
-						Quaternion.LookRotation(playerXZPosition - nguaXZPosition), turnSpeed * Time.deltaTime);
-				
-				if(distance < 6.5){ 
-					moveVector = Vector3.zero;
-				}
+    // Use this for initialization
+    void Start()
+    {
+        animator = gameObject.GetComponentInChildren<Animator>();
+        control = gameObject.GetComponent<CharacterController>();
 
-				if(distance < attackRange && Time.time > cooldownCounter)
-				{
-					int randnum = Random.Range(0, 3);
-					switch (randnum)
-					{
-						case 0: 
-							nguaAction = NguaAction.Dashing;
-							currentSpeed += 40;
-							currentDashTime = 0;
-							break;
-						case 1: 
-							nguaAction = NguaAction.Punching;
-							break;
-						case 2: 
-							nguaAction = NguaAction.TailAttack;
-							break;
-						default: nguaAction = NguaAction.Neutral; break;
-					}
-				}	
-			}
-			else if(nguaAction == NguaAction.Dashing)
-			{
-				moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
-				currentDashTime += 1;
+        player = GameObject.FindWithTag("Player");
+        nguaMoving = NguaMoving.Neutral;
+        currentSpeed = moveSpeed;
+        headCollider.enabled = false;
+    }
 
-				if(currentDashTime > maxDashTime){
-					animator.SetInteger("dashing", 0);
-					headCollider.enabled = false;
-					currentSpeed -= 40;
-					cooldownCounter = Time.time + cooldown;
-					nguaAction = NguaAction.Following;
-				}
-				else if(currentDashTime+(maxDashTime*0.2)> maxDashTime){
-					animator.SetInteger("dashing", 3);
-					headCollider.enabled = false;
-				}
-				else if(currentDashTime < 10){
-					animator.SetInteger("dashing", 1);
-					moveVector = Vector3.zero;
-					nguaXZPosition = new Vector3(transform.position.x, 0, transform.position.z);
-					playerXZPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
-					transform.rotation = Quaternion.Lerp(transform.rotation, 
-						Quaternion.LookRotation(playerXZPosition - nguaXZPosition), turnSpeed * Time.deltaTime);
-				}
-				else if(currentDashTime < maxDashTime){
-					animator.SetInteger("dashing", 2);
-					moveVector.y = ((player.transform.position.y-5) - transform.position.y) * Time.deltaTime;
-					headCollider.enabled = true;
-				}
-			}
-			else if(nguaAction == NguaAction.Punching)
+    // Update is called once per frame
+    void Update()
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        inRange = distance < followRange;
+        if (inRange)
+        {
+            //Debug.Log(nguaAction + " " + nguaMoving + ": " + distance);
+            if (distance < 6.5 && nguaMoving == NguaMoving.Following)
+            {
+                nguaMoving = NguaMoving.Neutral;
+            	currentDashTime = 0;
+            	animator.SetInteger("waking", 0);
+            	moveVector = Vector3.zero;
+            	currentSpeed = moveSpeed;
+            }
+			else if(distance > 7 && nguaMoving == NguaMoving.Neutral)
 			{
-				animator.SetInteger("attackMove", 1);
-				if(animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|Ngua_attack"))
-				{
-					animator.SetInteger("attackMove", 0);
-					nguaAction = NguaAction.Following;
-					cooldownCounter = Time.time + cooldown;
-				}
+				nguaMoving = NguaMoving.Following;
 			}
-			else if(nguaAction == NguaAction.TailAttack)
-			{
-				animator.SetInteger("attackMove", 2);
-				if(animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|Ngua_attack3"))
-				{
-					animator.SetInteger("attackMove", 0);
-					nguaAction = NguaAction.Following;
-					cooldownCounter = Time.time + cooldown;
-				}
-			}
-			else if(nguaAction == NguaAction.Neutral)
-			{
-				nguaAction = NguaAction.Following;
-			}
-		}
-		else if(!inRange)
-		{
-			nguaAction = NguaAction.Neutral;
-			currentDashTime = 0;
-			animator.SetInteger("waking", 0);
-			moveVector = Vector3.zero;
-			currentSpeed = moveSpeed;
-		}
-		moveVector.y -= gravity * Time.deltaTime;
-		control.Move(moveVector);
-	}
+        }
+        else if (!inRange)
+        {
+            nguaMoving = NguaMoving.Neutral;
+            currentDashTime = 0;
+            animator.SetInteger("waking", 0);
+            moveVector = Vector3.zero;
+            currentSpeed = moveSpeed;
+        }
+
+        if (nguaMoving == NguaMoving.Attacking)
+        {
+            if (nguaAction == NguaAction.Dashing)
+            {
+                moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
+                currentDashTime += 1;
+
+                if (currentDashTime > maxDashTime || NguaCollisionDetector.isColliding)
+                {
+                    animator.SetInteger("dashing", 0);
+                    NguaCollisionDetector.isColliding = false;
+                    headCollider.enabled = false;
+                    currentSpeed -= 40;
+                    dashCooldownCounter = Time.time + dashCooldown;
+                    nguaMoving = NguaMoving.Following;
+                    nguaAction = NguaAction.None;
+                }
+                else if (currentDashTime + (maxDashTime * 0.2) > maxDashTime)
+                {
+                    animator.SetInteger("dashing", 3);
+                    headCollider.enabled = false;
+                }
+                else if (currentDashTime < 10)
+                {
+                    animator.SetInteger("dashing", 1);
+                    moveVector = Vector3.zero;
+                    nguaXZPosition = new Vector3(transform.position.x, 0, transform.position.z);
+                    playerXZPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+                    transform.rotation = Quaternion.Lerp(transform.rotation,
+                        Quaternion.LookRotation(playerXZPosition - nguaXZPosition), turnSpeed * Time.deltaTime);
+                }
+                else if (currentDashTime < maxDashTime)
+                {
+                    animator.SetInteger("dashing", 2);
+                    moveVector.y = ((player.transform.position.y - 5) - transform.position.y) * Time.deltaTime;
+                    headCollider.enabled = true;
+                }
+            }
+            else if (nguaAction == NguaAction.TailAttack)
+            {
+                animator.SetInteger("attackMove", 2);
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|Ngua_attack3"))
+                {
+                    animator.SetInteger("attackMove", 0);
+                    nguaMoving = NguaMoving.Following;
+                    nguaAction = NguaAction.None;
+                    tailCooldownCounter = Time.time + tailCooldown;
+                }
+            }
+            else if (nguaAction == NguaAction.Slapping)
+            {
+                animator.SetInteger("attackMove", 1);
+                if (animator.GetCurrentAnimatorStateInfo(0).IsName("Armature|Ngua_attack"))
+                {
+                    animator.SetInteger("attackMove", 0);
+                    nguaMoving = NguaMoving.Following;
+                    nguaAction = NguaAction.None;
+                    slapCooldownCounter = Time.time + slapCooldown;
+                }
+            }
+        }
+
+        else if (nguaMoving == NguaMoving.Following)
+        {
+            animator.SetInteger("waking", 1);
+            moveVector = transform.TransformDirection(Vector3.forward) * currentSpeed * Time.deltaTime;
+            nguaXZPosition = new Vector3(transform.position.x, 0, transform.position.z);
+            playerXZPosition = new Vector3(player.transform.position.x, 0, player.transform.position.z);
+            transform.rotation = Quaternion.Lerp(transform.rotation,
+                    Quaternion.LookRotation(playerXZPosition - nguaXZPosition), turnSpeed * Time.deltaTime);
+
+            //add if inrange
+            if (distance < slapRange && Time.time > slapCooldownCounter)
+            {
+                nguaAction = NguaAction.Slapping;
+                nguaMoving = NguaMoving.Attacking;
+            }
+            else if (distance < tailRange && Time.time > tailCooldownCounter) 
+            {
+                nguaAction = NguaAction.TailAttack;
+                nguaMoving = NguaMoving.Attacking;
+            }
+            else if (distance < dashRange && Time.time > dashCooldownCounter ) 
+            {
+                currentSpeed += 40;
+                currentDashTime = 0;
+                nguaAction = NguaAction.Dashing;
+                nguaMoving = NguaMoving.Attacking;
+            }
+        }
+        
+        else if (nguaMoving == NguaMoving.Neutral)
+        {
+            currentDashTime = 0;
+            animator.SetInteger("waking", 0);
+            moveVector = Vector3.zero;
+            currentSpeed = moveSpeed;
+            if (distance < slapRange && Time.time > slapCooldownCounter)
+            {
+                nguaAction = NguaAction.Slapping;
+                nguaMoving = NguaMoving.Attacking;
+            }
+        }
+
+        moveVector.y -= gravity * Time.deltaTime;
+        control.Move(moveVector);
+    }
 }
---
+++ Assets/Scripts/AI/NguaCollisionDetector.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NguaCollisionDetector : MonoBehaviour {
+
+	public static bool isColliding = false;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) // || other.gameObject.CompareTag("Wall")
+		{
+			isColliding = true;
+		}
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+		{
+			isColliding = false;
+		}
+    }
+}
